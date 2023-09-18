7320f10572e1b3dfc92fdb7c9d5c526ceedec66f
verbalins <simon.lidberg@gmail.com>
2023-09-18 14:31:08 +0200

    Additional changes to graph display
    Changed the number of rows displayed in the legend of the metric graphs. Clarified the units of the settings for alpha.

---
+++ CreateVisualizations.R
@@ -0,0 +1,154 @@
+source("R/VisualizeData.R")
+#library(xtable)
+library(kableExtra)
+library(Hmisc)
+library(patchwork)
+library(flextable)
+
+# Export plots
+save_plot <- function(x, p) {
+  ggsave(
+    filename = paste0("img/", x, ".pdf"),
+    plot = p,
+    device = cairo_pdf,
+    units = "px",
+    height = 2500,
+    width = 2000,
+  )
+}
+
+rmse <- function(obs, pred) {
+  sqrt(sum((obs - pred)^2)/length(obs))
+}
+
+nrmse <- function(obs, pred) {
+  rmse(obs, pred) / mean(obs)
+}
+
+
+# Load and transform data
+df <- dplyr::bind_rows(get_data_from_db(db = "data/Detailed.db") %>%
+                         mutate(ExpName = "Detailed") %>%
+                         add_experiment_parameters(),
+                       get_data_from_db(db = "data/Simplified.db") %>%
+                         mutate(ExpName = "Simplified") %>%
+                         add_experiment_parameters())
+
+data_with_errors <- df %>%
+  filter(BufferSize > 0) %>%
+  select(PT:ExpName,-all_of(c("ID", "StatTHP", "TargetMDT", "Runtime"))) %>%
+  pivot_longer(cols = LT_avg:JPH_sd, names_to = "Variable") %>%
+  pivot_wider(names_from = ExpName, values_from = value) %>%
+  nest_by(Experiment, Variable) %>%
+  summarise(NRMSE = nrmse(data$Detailed, data$Simplified)) %>%
+  pivot_wider(names_from = Variable, values_from = NRMSE)
+
+data_for_table <- data_with_errors %>%
+  ungroup() %>%
+  right_join(y=df %>%
+               filter(BufferSize > 0) %>%
+               select(Experiment, NumberMachines, Setting, Runtime, ExpName, Observation) %>%
+               pivot_wider(names_from=ExpName, values_from=Runtime, names_glue="Runtime_{ExpName}") %>%
+               group_by(Setting, NumberMachines, Experiment) %>%
+               summarise(across(where(is.double), mean))) %>%
+  group_by(Setting,
+           NumberMachines = cut(NumberMachines,
+                                breaks = c(5,25,50,100,200,350,500), include.lowest=TRUE, labels=c("5-25", "25-50", "50-100", "100-250", "250-350", "350-500"))) %>%
+  summarise(across(contains(c("_", "Runtime")), mean)) %>%
+  select(Setting, NumberMachines, contains(c("_avg","Runtime"))) %>%
+  ungroup() %>%
+  left_join(y = df %>%
+              select(PT:Setting) %>%
+              distinct()) %>%
+  relocate(PT:MDT, .after = Setting)
+
+kable(data_for_table,
+      format = "latex",
+      booktabs = TRUE,
+      caption = "\\gls{nrmse} for average values of \\gls{jph}, \\gls{lt}, and \\gls{wip}, binned by $\\beta$. Average Runtime values in seconds for \\textit{Detailed} and \\textit{Simplified}. The results are only for $\\gamma > 0$",
+      digits = 3,
+      label = "average_results",
+      col.names = c("$\\alpha$", "PT", "Avb", "MDT", "$\\beta$", "JPH", "LT", "WIP", "Detailed", "Simplified"),
+      table.envir = "table*",
+      align = "lllllrrrrr") %>%
+  collapse_rows(columns=c(1,2,3,4), latex_hline = "linespace", target=1) %>%
+  add_header_above(c("$\\alpha$" = 4, " " = 1, "NRMSE (\\SI{}{\\percent})" = 3, "Average Runtime (s)" = 2)) %>%
+  save_kable(file = "img/experimenttable.tex")
+
+grouped <- df %>%
+  group_data() %>%
+  arrange(Experiment)
+
+compute_rmse <- function(df, attr) {
+  df %>%
+    select(Setting:BufferSize, -all_of(c("ID", "StatTHP", "TargetMDT")), all_of({{attr}}), ExpName) %>%
+    pivot_wider(names_from = ExpName, values_from = {{attr}}) %>%
+    nest_by(Setting, Experiment, BufferSize, NumberMachines) %>%
+    mutate(MAE = mean(abs(data$Detailed - data$Simplified)),
+           RMSE = rmse(data$Detailed, data$Simplified),
+           NRMSE = nrmse(data$Detailed, data$Simplified))
+}
+
+compute_error_and_save <- function(data, g, filename="", metric = "NRMSE") {
+  invisible(lapply(data |> select(LT_avg:JPH_sd) |> colnames(),
+                   function(x) save_plot(paste0(x, filename), compute_rmse(data, x) |>
+                                           plot_compare_error_combined(attr=x, metric=metric, g = g))))
+}
+
+ex_table <- df %>%
+  select(PT:MDT) %>%
+  distinct() %>%
+  t()
+
+alpha_unicode <-
+  c("\U03b1\U2081",
+    "\U03b1\U2082",
+    "\U03b1\U2083",
+    "\U03b1\U2084",
+    "\U03b1\U2085")
+
+colnames(ex_table) <- alpha_unicode
+
+ex_table_grob <-
+  flextable(data = ex_table |>
+              as_tibble(rownames = " ") |>
+              mutate(' ' = c("PT (s)", "Avb (%)", "MDT (s)"))) |>
+  align(align = "center", part = "all") |>
+  align(j = 1, align = "left", part = "all") |>
+  gen_grob(scaling = "full")
+
+df |> compute_error_and_save(g = ex_table_grob,
+                 filename="_500") # Default 500 beta and all gamma
+
+df |> # Only 200 beta
+  filter(NumberMachines <= 200) |>
+  compute_error_and_save(g = ex_table_grob,
+             filename="_200")
+
+df |> # 200 beta and gamma > 0
+  filter(NumberMachines <= 200,
+         BufferSize != 0) |>
+  compute_error_and_save(g = ex_table_grob,
+             filename="_200_0gamma")
+
+# Runtime graph
+p <- grouped %>%
+  mutate(Setting = factor(Setting,
+                          levels = c("1","2","3","4","5"),
+                          labels = alpha_unicode),
+         ExpName = str_replace(ExpName, "Aggregated", "Simplified"),
+         ExpName = factor(ExpName,
+                          levels = c("Simplified", "Detailed"))) %>% # Alpha_n
+  ggplot(aes(NumberMachines, Runtime, group = interaction(ExpName, BufferSize), color = ExpName)) +
+  geom_line(alpha=0.4) +
+  geom_smooth(aes(group=ExpName), se = FALSE) +
+  scale_y_continuous(breaks = seq(0, 200, by = 50), limits = c(0, 250)) +
+  scale_color_discrete(breaks = c('Detailed', 'Simplified')) +
+  facet_wrap(~Setting, nrow = 5, strip.position = "right", drop = TRUE) +
+  ylab("Runtime in seconds") +
+  xlab(paste("Number of buffer/machine pairs in sequence,", "\u03b2")) +
+  guides(color=guide_legend(title="Model type")) +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "bottom")
+
+save_plot("Runtime", p)
---
+++ VisualizeData.R
@@ -0,0 +1,236 @@
+source("R/AnalyzeData.R")
+
+# How to compare, we have several different KPIs. Facet on input distribution.
+# Create a visualisation for each metric with min, avg, and max.
+temp <- function(df) {
+  df %>% filter(InputDistribution == 0) %>%
+    plot_ly(x = ~Experiment, name = "LT avg.", type = "scatter", mode = "lines",
+            line = list(color = "red", width = 4)) %>%
+    add_trace(y = ~LT_min, name = "LT min",
+              line = list(color="red", width = 4, dash = "dash")) %>%
+    add_trace(y = ~LT_max, name = "LT max",
+              line = list(color="red", width = 4, dash = "dash"))
+
+  grouped %>%
+    filter(InputDistribution == 0) %>%
+    select(starts_with("LT"), ExpName) %>%
+    ggplot(aes(Experiment, y = LT_avg, color = ExpName)) + geom_point()
+}
+
+parallell_coordinates <- function(df) {
+  df %>% filter(Runtime > 0) %>%
+    plot_ly(type = 'parcoords',
+            line = list(color = ~Runtime,
+                        colorscale = 'Jet',
+                        showscale = FALSE,
+                        reversescale = FALSE),
+            dimensions = list(
+              list(range = c(1,3),
+                   tickvals = c(1,2,3),
+                   ticktext = c("Detailed", "Aggregated", "AggregatedPlace"),
+                   label = "Exp Type", values = ~as.numeric(ExpName)),
+              list(range = c(~min(NumberMachines),~max(NumberMachines)),
+                   label = 'Number Machines', values = ~NumberMachines),
+              list(range = c(~min(BufferSize),~max(BufferSize)),
+                   label = "Buffer Size", values = ~BufferSize),
+              list(range = c(~min(Runtime),~max(Runtime)),
+                   label = "Runtime", values = ~Runtime)
+            )
+    )
+}
+
+plotly_splom <- function(df) {
+
+  pl_colorscale=list(c(0.0, '#19d3f3'),
+                     c(0.333, '#19d3f3'),
+                     c(0.333, '#e763fa'),
+                     c(0.666, '#e763fa'),
+                     c(0.666, '#636efa'),
+                     c(1, '#636efa'))
+
+  axis = list(showline=FALSE,
+              zeroline=FALSE,
+              gridcolor='#ffff',
+              ticklen=4)
+
+  df %>%
+    filter(Experiment <= 1000) %>%
+    plot_ly() %>%
+    add_trace(type = "splom",
+              dimensions = list(
+                list(label = "LT_min", values = ~LT_min),
+                list(label = "LT_avg", values = ~LT_avg),
+                list(label = "LT_max", values = ~LT_max),
+                list(label = "NrMachines", values = ~NumberMachines)
+              ),
+              text = ~ExpName,
+              marker = list(
+                color = as.integer(df$ExpName),
+                colorscale = pl_colorscale,
+                size = 5,
+                line = list(
+                  width = 1,
+                  color = 'rgb(230,230,230)'
+                )
+              )
+    ) %>%
+    layout(
+      title= 'Comparison',
+      hovermode='closest',
+      dragmode= 'select',
+      plot_bgcolor='rgba(240,240,240, 0.95)',
+      xaxis=list(domain=NULL, showline=F, zeroline=F, gridcolor='#ffff', ticklen=4),
+      yaxis=list(domain=NULL, showline=F, zeroline=F, gridcolor='#ffff', ticklen=4),
+      xaxis2=axis,
+      xaxis3=axis,
+      xaxis4=axis,
+      yaxis2=axis,
+      yaxis3=axis,
+      yaxis4=axis
+    )
+}
+
+plot_diff <- function(attr, df, inputDist = 0, interactive = FALSE) {
+  if (!(inputDist == 0)) {
+    df <- df %>% filter(as.numeric(InputDistribution == inputDist))
+  }
+
+  gfx <- df %>%
+    filter(!ExpName == "Detailed") %>%
+    ggplot(aes(NumberMachines, y = !!as.name(attr), color = ExpName, group = interaction(Experiment, ExpName))) +
+    geom_point(alpha = 0.5, size = 0.2) +
+    stat_smooth(aes(group = ExpName), se = F) +
+    geom_hline(yintercept = 1) +
+    #scale_y_continuous(limits = c(0.5, 1.5)) +
+    scale_x_continuous(minor_breaks = seq(50, 450, by = 50)) +
+    #coord_cartesian(ylim = limits) + # Use the same zoom on all the graphs.
+    facet_wrap(~InputDistribution, nrow = 3, strip.position = "right", drop = TRUE) +
+    labs(title = attr, x = "Number of buffer/machine pairs in sequence", y = "% difference to Detailed",
+         color = "Experiment") +
+    theme_bw() +
+    theme(legend.position = "bottom")
+
+  if (!interactive) {
+    print(gfx)
+  }
+  else{
+    ggplotly(gfx)
+  }
+}
+
+compare_buffersize <- function(attr, df, interactive=FALSE) {
+  gfx <- df %>%
+    filter(!ExpName == "Detailed") %>%
+    ggplot(aes(NumberMachines, y = !!as.name(attr), group = interaction(ExpName, as.factor(BufferSize)))) +
+    geom_point(alpha = 0.5, size = 1, aes(shape = ExpName)) +
+    geom_hline(yintercept = 1) +
+    geom_line(aes(color = as.factor(BufferSize), group = interaction(ExpName, as.factor(BufferSize)))) +
+    #stat_smooth(aes(color = BufferSize), se = FALSE) +
+    scale_x_continuous(minor_breaks = seq(50, 450, by = 50)) +
+    facet_wrap(~InputDistribution, nrow = 3, strip.position = "right", drop = TRUE) +
+    labs(title = paste("BufferSize compared to", attr),
+         x = "Number of buffer/machine pairs in sequence",
+         y = "% difference to Detailed",
+         color = "BufferSize",
+         shape = "Experiment") +
+    theme_bw() +
+    theme(legend.position = "bottom")
+
+  if (!interactive) {
+    print(gfx)
+  }
+  else{
+    ggplotly(gfx)
+  }
+}
+
+compare_singleinputdist_buffersize <- function(attr, df, interactive=FALSE) {
+  df %>% mutate(BufferSize = as.factor(BufferSize)) %>%
+    filter(ExpName == "Aggregated") %>%
+    ggplot(aes(NumberMachines, y = !!as.name(attr), group = interaction(ExpName, BufferSize))) +
+    geom_point(alpha = 0.5, size = 1)+#, aes(shape = ExpName)) +
+    geom_hline(yintercept = 0) +
+    geom_line(aes(color = BufferSize, group = interaction(ExpName, BufferSize))) +
+    scale_x_continuous(minor_breaks = seq(50, 450, by = 50)) +
+    facet_wrap(vars(Setting), nrow = 5, strip.position = "right", drop = TRUE) +
+    labs(#title = paste("BufferSize compared to", attr),
+      x = "Number of buffer/machine pairs in sequence",
+      y = "Error",
+      color = "BufferSize",
+      shape = "Experiment") +
+    theme_bw(base_size = 18) +
+    theme(legend.position = "bottom")
+}
+
+get_legend <- function(myggplot) {
+  tmp <- ggplot_gtable(ggplot_build(myggplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
+}
+
+plot_compare_error <- function(df, attr, metric = "Error", interactive = FALSE) {
+  df %>% mutate(BufferSize = as.factor(BufferSize),
+                Setting = factor(Setting,
+                                 levels = c("1","2","3","4","5"),
+                                 labels = c("\U03b1\U2081",
+                                            "\U03b1\U2082",
+                                            "\U03b1\U2083",
+                                            "\U03b1\U2084",
+                                            "\U03b1\U2085"))) %>% # Alpha_n
+    ggplot(aes(NumberMachines, y = !!as.name(metric), group = interaction(Setting, BufferSize))) +
+    #geom_point(alpha = 0.5, size = 1) + #, aes(shape = ExpName)) +
+    geom_hline(yintercept = if_else(metric == "Ratio", 1, 0)) +
+    geom_line(aes(color = BufferSize, group = interaction(Setting, BufferSize))) +
+    scale_x_continuous(minor_breaks = seq(50, 450, by = 50)) +
+    scale_y_continuous(labels = scales::percent_format(accuracy = 1)) + # Use percentage
+    facet_wrap(vars(Setting), nrow = 5, strip.position = "right", drop = TRUE) +
+    labs(x = "Number of buffer/machine pairs in sequence, \u03b2", # Beta
+         y = paste(if_else(metric=="NRMSE", "Normalized RMSE for average", metric), unlist(strsplit(attr, "[_]"))[1]),
+         color = "\u03b3", # Gamma
+         shape = "Experiment") +
+    guides(color = guide_legend(nrow = 3, byrow = TRUE)) +
+    theme_bw(base_size = 14) +
+    theme(legend.position = "bottom",
+          legend.justification = "left")
+}
+
+plot_compare_error_combined <- function(df, attr, g, metric = "Error") {
+  p <- plot_compare_error(df, attr, metric)
+
+  p_legend <- get_legend(p)
+  p_no_legend <- p + theme(legend.position = "none")
+  p_no_legend / (wrap_elements(p_legend) | wrap_elements(g)) + plot_layout(heights = c(6,1))
+}
+
+plot_compare_error_ggpubr <- function(df, attr, metric = "Error") {
+  df %>% mutate(BufferSize = as.factor(BufferSize),
+                Setting = factor(Setting,
+                                 levels = c("1","2","3","4","5"),
+                                 labels = c("\u03b1\u2081",
+                                            "\u03b1\u2082",
+                                            "\u03b1\u2083",
+                                            "\u03b1\u2084",
+                                            "\u03b1\u2085"))) %>%
+    ggpubr::ggline(x = "NumberMachines", y = metric, group = "BufferSize", color = "BufferSize",
+                   plot_type = "l", ggtheme = theme_gray()) %>%
+    ggpubr::facet(facet.by = "Setting", nrow = 5, strip.position = "right") +
+    #ggpubr::border(color = "grey", size = 0.5) +
+    ggpubr::yscale("percent")
+
+}
+
+matrix_plot <- function(attr, data) {
+  cols <- regmatches(colnames(data), regexpr(paste0(attr,"_[a-z]*"), colnames(data)))
+  input1 <- lapply(cols, plot_diff, df = data, inputDist = 1)
+  input2 <- lapply(cols, plot_diff, df = data, inputDist = 2)
+  input3 <- lapply(cols, plot_diff, df = data, inputDist = 3)
+
+  ggmatrix(do.call(c, list(input1, input2, input3)), nrow = 3, ncol = length(input1), byrow = TRUE,
+           xAxisLabels = cols,
+           showAxisPlotLabels = TRUE,
+           yAxisLabels = levels(grouped$InputDistribution),
+           legend = c(1,1),
+           title = attr) +
+    theme(legend.position = "bottom")
+}
